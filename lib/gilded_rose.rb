508694004443705d634938febcb822dfb7e7e90d
Pav Rao <pav0107@gmail.com>
2021-09-02 13:54:44 +0100

    isolated 'Backstage passes to a TAFKAL80ETC concert'

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,40 +10,34 @@ class GildedRose
         if item.quality > 0
             item.quality -= 1 
         end
+        item.sell_in -= 1
       end
       
       if item.name == "Aged Brie"
         if item.quality < 50
           item.quality += 1
         end
-      end
-
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        # if item.quality > 0
-        #   if item.name != "Sulfuras, Hand of Ragnaros"
-        #     item.quality = item.quality - 1
-        #   end
-        # end
-      else
-        if item.quality < 50
-          item.quality += 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in -= 1
       end
+
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality < 50
+          item.quality += 1
+        end
+        if item.sell_in < 11
+          if item.quality < 50
+            item.quality += 1
+          end
+        end
+        if item.sell_in < 6
+          if item.quality < 50
+            item.quality += 1
+          end
+        end
+        item.sell_in -= 1
+      end
+
+
       if item.sell_in < 0
         if item.name != "Backstage passes to a TAFKAL80ETC concert"
           if item.quality > 0
