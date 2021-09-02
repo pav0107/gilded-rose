2fc20974bd727af58d40aafd0a6c7fc5c7366c0f
Pav Rao <pav0107@gmail.com>
2021-09-02 16:58:34 +0100

    created 3 new methods: normal, brie and backstage

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,29 +4,38 @@ class GildedRose
     @items = items
   end
 
+  def update_quality_normal(item)
+    item.sell_in -= 1
+      if item.quality > 0
+          item.quality -= 1
+          item.quality -= 1 if item.sell_in < 0 
+      end
+  end
+
+  def update_quality_brie(item)
+    item.sell_in -= 1
+    item.quality += 1 if item.quality < 50
+  end
+
+  def update_quality_backstate(item)
+    if item.quality < 50
+      item.quality += 1
+      item.quality += 1 if item.sell_in < 11
+      item.quality += 1 if item.sell_in < 6
+    end
+    item.sell_in -= 1
+    item.quality = 0 if item.sell_in < 0
+  end
+  
+
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in -= 1
-        if item.quality > 0
-            item.quality -= 1
-            item.quality -= 1 if item.sell_in < 0 
-        end
-      end
-      
-      if item.name == "Aged Brie"
-        item.sell_in -= 1
-        item.quality += 1 if item.quality < 50
-      end
-
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality < 50
-          item.quality += 1
-          item.quality += 1 if item.sell_in < 11
-          item.quality += 1 if item.sell_in < 6
-        end
-        item.sell_in -= 1
-        item.quality = 0 if item.sell_in < 0
+        update_quality_normal(item)
+      elsif item.name == "Aged Brie"
+        update_quality_brie(item)
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        update_quality_backstate(item)
       end
     end
   end
