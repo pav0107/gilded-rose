8502a2b27024ddbd441032107f7c4534d2b0068a
Pav Rao <pav0107@gmail.com>
2021-09-03 13:34:06 +0100

    added Sulfuras class and refactored update_quality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,45 +4,9 @@ class GildedRose
     @items = items
   end
 
-  # def update_quality_normal(item)
-  #   item.sell_in -= 1
-  #     if item.quality > 0
-  #         item.quality -= 1
-  #         item.quality -= 1 if item.sell_in < 0 
-  #     end
-  # end
-
-  # def update_quality_conjured(item)
-  #   item.sell_in -= 1
-  #     if item.quality > 0
-  #         item.quality -= 2
-  #         item.quality -= 2 if item.sell_in < 0 
-  #     end
-  # end
-
-  # def update_quality_brie(item)
-  #   item.sell_in -= 1
-  #   item.quality += 1 if item.quality < 50
-  # end
-
-  # def update_quality_backstage(item)
-  #   if item.quality < 50
-  #     item.quality += 1
-  #     item.quality += 1 if item.sell_in < 11
-  #     item.quality += 1 if item.sell_in < 6
-  #   end
-  #   item.sell_in -= 1
-  #   item.quality = 0 if item.sell_in < 0
-  # end
-  
-
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros" && item.name != "Conjured"
-        # update_quality_normal(item)
-        normal = Normal.new(item)
-        normal.update_quality(item)
-      elsif item.name == "Conjured"
+      if item.name == "Conjured"
         conjured = Conjured.new(item)
         conjured.update_quality(item)
       elsif item.name == "Aged Brie"
@@ -51,6 +15,12 @@ class GildedRose
       elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
         backstage = Backstage.new(item)
         backstage.update_quality(item)
+      elsif item.name == "Sulfuras, Hand of Ragnaros"
+        backstage = Sulfuras.new(item)
+        backstage.update_quality(item)
+      else
+        normal = Normal.new(item)
+        normal.update_quality(item)
       end
     end
   end
@@ -107,6 +77,15 @@ class Brie
   def update_quality(item)
     item.sell_in -= 1
     item.quality += 1 if item.quality < 50
+  end
+end
+
+class Sulfuras
+  def initialize(items)
+    @items = items
+  end
+
+  def update_quality(item)
   end
 end
 
